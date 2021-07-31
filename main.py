afcf24e21cef31f6dbd4cfe7a28811d710b4c560
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-07-31 22:44:52 +0530

    updated start texts

--- main.py
+++ main.py
@@ -3,9 +3,11 @@
 import os
 
 from pyrogram import Client, idle
-from config import Config
+from config import Config, B_START_TEXT, MEGA_LOGIN_TEXT, START_TEXT
+from megadl.account import login_to_mega
 
 if __name__ == "__main__" :
+    print(B_START_TEXT)
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(root="megadl")
@@ -16,5 +18,8 @@ if __name__ == "__main__" :
         api_hash=Config.API_HASH,
         plugins=plugins
     )
+    print(MEGA_LOGIN_TEXT)
+    login_to_mega()
     app.start()
+    print(START_TEXT)
     idle()
