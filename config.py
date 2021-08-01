1f97e570c8d4f8bd2c75463d34c3448a7a0b6f65
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-08-02 00:13:48 +0530

    new error logs

--- config.py
+++ config.py
@@ -3,16 +3,17 @@
 import os
 
 class Config(object):
-    APP_ID = int(os.environ.get("APP_ID", 123456))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    APP_ID = int(os.environ.get("APP_ID", 2294033))
+    API_HASH = os.environ.get("API_HASH", "6a8fba10d5e2747b3c101c03d2d22752")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "1948020257:AAGYFIg1kdt6BTsTyvsZdWQ861VTq-AsdCo")
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1340254734").split())
     DOWNLOAD_LOCATION = "./NexaBots"
+    IS_PUBLIC_BOT = os.environ.get("IS_PUBLIC_BOT", "True")
     TG_MAX_SIZE = 2040108421
-    IS_PUBLIC_BOT = os.environ.get("IS_PUBLIC_BOT", "")
     # Mega User Account
-    MEGA_EMAIL = os.environ.get("MEGA_EMAIL", "")
-    MEGA_PASSWORD = os.environ.get("MEGA_PASSWORD", "")
+    MEGA_EMAIL = os.environ.get("MEGA_EMAIL", "tg.nexabots@gmail.com")
+    MEGA_PASSWORD = os.environ.get("MEGA_PASSWORD", "hirusha2006")
+
 
 # Text Prints
 B_START_TEXT = """
@@ -38,6 +39,7 @@ ERROR_TEXT = """
 | |___| |  | | | (_) | |   
 |_____|_|  |_|  \___/|_|  
 
+Log: {}
 
 Save the Log file and Send it to @Nexa_bots for Help :)
 """
